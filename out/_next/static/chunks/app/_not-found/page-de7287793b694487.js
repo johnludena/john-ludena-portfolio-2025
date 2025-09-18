ae87da79f65c9bee212df1d298714b72c65f334c
John Ludena <johnludena@gmail.com>
2025-09-18 14:24:42 -0500

    Adding back 'out' folder from Next.js build output for Coolify to deploy.

---
+++ out/_next/static/chunks/app/_not-found/page-de7287793b694487.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[409],{1867:function(n,e,_){Promise.resolve().then(_.t.bind(_,7455,23))},7455:function(n){n.exports={style:{fontFamily:"'__Syne_744198', '__Syne_Fallback_744198'",fontStyle:"normal"},className:"__className_744198"}}},function(n){n.O(0,[39,971,117,744],function(){return n(n.s=1867)}),_N_E=n.O()}]);
